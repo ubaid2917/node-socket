2fb7a4fc1729cbdc17ac4e82ba5a013c2e524065
ubaid naeem <ubaid29170@gmail.com>
2025-10-07 23:59:46 +0500

    hello world print commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express');
-const http = require('http');
+const app = require('express')();
+const http = require('http').Server(app);
+const path = require('path'); 
+const io = require('socket.io')(http);
 
-server.listen(3000, () => {
+ app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+ })  
+
+ io.on('connection', (socket) => {
+    console.log('A user connected', socket.id); 
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected', socket.id);
+    })
+ })
+
+http.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
